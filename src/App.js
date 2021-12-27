fbbff870742d04a8ebd4920e3f407bd649e40d93
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2021-12-27 13:55:50 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+        <meta name="description" content="traffic light stuff"/>
+        <meta name="keywords" content="HTML,CSS,JavaScript"/>
+      </head>
+      <body>
+        <div className="redLight">
+
+        </div>
+        <div className="yellowLight">
+
+        </div>
+        <div className="greenLight">
+
+        </div>        
+      </body>
     </div>
   );
 }
