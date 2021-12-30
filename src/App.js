2aa29c9657d267de6ecfe5860c0c6cee4f190e7f
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2021-12-29 17:33:21 -0800

    v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import './App.css';
-
+import React, { useState } from 'react';
 function App() {
+  var timeInterval;
+  //const [counter, setCounter] = useState(0);
+  var counter = 0;
+  const startLights = () =>{
+    let reds = document.querySelectorAll(".redLight");
+    let yellows = document.querySelectorAll(".yellowLight");
+    let greens = document.querySelectorAll(".greenLight");
+    document.getElementById("buttonId").hidden = true;
+    timeInterval = setInterval(() =>{
+      for(let i = 0; i < reds.length; i++){
+        if((i+counter)%3 === 0){
+          yellows[i].style.backgroundColor = "black";
+          greens[i].style.backgroundColor = "black";
+          reds[i].style.backgroundColor = "red";
+        }
+        else if((i+counter)%3 === 1){
+          yellows[i].style.backgroundColor = "yellow";
+          greens[i].style.backgroundColor = "black";
+          reds[i].style.backgroundColor = "black";
+        }
+        else if((i+counter)%3 === 2){
+          yellows[i].style.backgroundColor = "black";
+          greens[i].style.backgroundColor = "green";
+          reds[i].style.backgroundColor = "black";
+        }
+      }
+      counter++;
+      counter %= 3;
+    }, 500);
+  }
+  const repeat = [...Array(432)].map( (v) =>
+        <div className="repeatFunction">
+          <div className="redLight"></div>
+          <div className="yellowLight"></div>
+          <div className="greenLight"></div>
+        </div>
+      );
+  
   return (
     <div className="App">
       <head>
@@ -8,15 +46,8 @@ function App() {
         <meta name="keywords" content="HTML,CSS,JavaScript"/>
       </head>
       <body>
-        <div className="redLight">
-
-        </div>
-        <div className="yellowLight">
-
-        </div>
-        <div className="greenLight">
-
-        </div>        
+        <div className = "rowFormation">{repeat}</div>
+        <button type="button" id="buttonId" onClick={startLights}>b u t t o n</button>
       </body>
     </div>
   );
